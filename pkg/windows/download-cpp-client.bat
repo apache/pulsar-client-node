b274e33eef776df37d4aec547ad2d4f991424c25
Baodi Shi <wudixiaolongbao@icloud.com>
2022-12-04 11:00:34 +0800

    Fix windows default download cpp client failed.

--- pkg/windows/download-cpp-client.bat
+++ pkg/windows/download-cpp-client.bat
@@ -1,8 +1,12 @@
 cd %~dp0
 set arch=%1
+if "%arch%" == "" (
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set arch=x86 || set arch=x64
+)
 set /P BASE_URL=<..\..\build-support\cpp-base-url.txt
 curl -O -L %BASE_URL%/%arch%-windows-static.tar.gz
 tar -xvzf %arch%-windows-static.tar.gz
-mv %arch%-windows-static pulsar-cpp
+move %arch%-windows-static pulsar-cpp
 dir
+
 
