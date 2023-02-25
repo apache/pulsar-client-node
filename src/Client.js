5453e03e37b4dbf0e4010e0fc4c2bb2c5f2989b7
Baodi Shi <baodi@apache.org>
2023-02-25 15:26:53 +0800

    Use the certificate provided by the node.js (#301)
    * Use the certificate provided by the node.js.
    
    * Gen cert file when npm install.
    
    * Certificate generation is performed only once
    
    * Fix code reviews.
    
    (cherry picked from commit a89b64fffc44cca2663b4caf954839e6cab2a3d8)

---
+++ src/Client.js
@@ -0,0 +1,68 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+const fs = require('fs');
+const tls = require('tls');
+const os = require('os');
+const PulsarBinding = require('./pulsar-binding');
+
+const certsFilePath = `${__dirname}/cert.pem`;
+
+class Client {
+  constructor(params) {
+    if (!params.tlsTrustCertsFilePath) {
+      // eslint-disable-next-line no-param-reassign
+      params.tlsTrustCertsFilePath = certsFilePath;
+    }
+    this.client = new PulsarBinding.Client(params);
+  }
+
+  createProducer(params) {
+    return this.client.createProducer(params);
+  }
+
+  subscribe(params) {
+    return this.client.subscribe(params);
+  }
+
+  createReader(params) {
+    return this.client.createReader(params);
+  }
+
+  close() {
+    this.client.close();
+  }
+
+  static setLogHandler(params) {
+    PulsarBinding.Client.setLogHandler(params);
+  }
+
+  static genCertFile() {
+    fs.rmSync(certsFilePath, { force: true });
+    const fd = fs.openSync(certsFilePath, 'a');
+    try {
+      tls.rootCertificates.forEach((cert) => {
+        fs.appendFileSync(fd, cert + os.EOL);
+      });
+    } finally {
+      fs.closeSync(fd);
+    }
+  }
+}
+
+module.exports = Client;
